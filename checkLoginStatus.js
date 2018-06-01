b0ae0532bc9a4cf3bceee8e19c07e3e121dd2414
garouttc <39816619+garouttc@users.noreply.github.com>
2018-05-31 22:16:52 -0700

    Update checkLoginStatus.js

--- checkLoginStatus.js
+++ checkLoginStatus.js
@@ -9,25 +9,25 @@ if (typeof(Storage) == "undefined")
 // If a user is logged in show the searching, finding, and view pet profiles options
 if (sessionStorage.getItem("username"))
 {
-  document.getElementById("jumbotron-content").innerHTML = "                
+  document.getElementById("jumbotron-content").innerHTML = '                
                 <button id="btn_download"class="btn btn-primary btn btn-lg">Search for Lost Pet</button>
                 <script type="text/javascript">$("#btn_download").click(function(){window.open('/download');})</script>
                 <button id="btn_download"class="btn btn-primary btn btn-lg">Report a Lost Pet</button>
                 <script type="text/javascript">$("#btn_download").click(function(){window.open('/download');})</script>
                 <button id="btn_download"class="btn btn-primary btn btn-lg">My Pet Profile</button>
                 <script type="text/javascript">$("#btn_download").click(function(){window.open('/download');})</script>
-                ";
+                ';
 }
 // Otherwise, provide a login prompt
 else
 {
-  document.getElementById(jumbotron-content".innerHTML = "
+  document.getElementById(jumbotron-content".innerHTML = '
     <form>
       Username <input type="text" name="username">
       Password <input type="password" name="password">
       <input type="submit" action="index.html" method="post" value="Submit">
     </form>
-  ";
+  ';
   
 }
   
