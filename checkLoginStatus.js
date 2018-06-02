254afb93a0c12a9c8dd5ac064d4a62bd6ed00251
garouttc <39816619+garouttc@users.noreply.github.com>
2018-06-01 21:26:08 -0700

    Update checkLoginStatus.js

--- checkLoginStatus.js
+++ checkLoginStatus.js
@@ -14,18 +14,19 @@ window.onload = function()
     // Show the searching, finding, and view pet profiles options
     document.getElementById("jumbotron-content").innerHTML = '<button id="btn_download"class="btn btn-primary btn btn-lg">Search for Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">Report a Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">My Pet Profile</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>';
     
-    // As well as in the navbar
-    document.getElementById("bs-navbar-collapse-main").innerHTML = '<li><a href="/index.html">Home</a></li>\n<li><a href="/search.html">Search for a Pet</a></li>\n<li><a href="/report-found.html">Report a Lost Pet</a></li>\n<li><a href="/pet-profiles.html">My Profile</a></li>\n<li><a href="/pet-profiles.html">My Pet Profiles</a></li>';
-
   }
   // If a user is not logged in
   else
   {
     // Show login prompt
-    document.getElementById("jumbotron-content").innerHTML = '<form>\nUsername <input type="text" name="username">\nPassword <input type="password" name="password">\n<input type="submit" action="index.html" method="post" value="Submit">\n</form>';
+    document.getElementById("jumbotron-content").innerHTML = '<form id="login-form">\nUsername <input type="text" name="username">\nPassword <input type="password" name="password">\n<input type="submit" action="index.html" method="post" value="Submit">\n</form>';
     
-    // Clear the navbar options for everything except homepage
+    // Hide navbar options that shouldn't be accessed from users who are not logged in
     document.getElementById("bs-navbar-collapse-main").style.display = 'none';
+    
+    // Github pages only allows modification of client side data, and hence we can't use PHP to get POST variables from login form
+    // So, create an event listener for submitting the form that grabs input variables, stores them, and creates sessionStorage
+    document.getElementById("login-form").addEventListener("submit", function(event){ event.preventDefault(); return false; });
 
   }
 }
