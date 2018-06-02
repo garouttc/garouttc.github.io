92b954c155e729c1d73023cf5f10f7238e6b0a39
garouttc <39816619+garouttc@users.noreply.github.com>
2018-06-01 21:37:13 -0700

    Update checkLoginStatus.js

--- checkLoginStatus.js
+++ checkLoginStatus.js
@@ -1,3 +1,20 @@
+function getLoginVars(event)
+{
+  // Stop form submission, since we can't use PHP :(
+  event.preventDefault(); 
+  
+  var username = document.getElementById("login-form").elements["username"];
+  var password = document.getElementById("login-form").elements["password"];
+  
+  // Show the searching, finding, and view pet profiles options
+  document.getElementById("jumbotron-content").innerHTML = '<button id="btn_download"class="btn btn-primary btn btn-lg">Search for Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">Report a Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">My Pet Profile</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>';
+  
+  // Show navbar options again
+  document.getElementById("bs-navbar-collapse-main").style.display = 'none';
+  
+  return [username, password];
+}
+
 window.onload = function()
 {
   // First check if the browser supports session storage
@@ -26,7 +43,8 @@ window.onload = function()
     
     // Github pages only allows modification of client side data, and hence we can't use PHP to get POST variables from login form
     // So, create an event listener for submitting the form that grabs input variables, stores them, and creates sessionStorage
-    document.getElementById("login-form").addEventListener("submit", function(event){ event.preventDefault(); return false; });
+    var loginDetails;
+    document.getElementById("login-form").addEventListener("submit", loginDetails = getLoginVars(event));
 
   }
 }
