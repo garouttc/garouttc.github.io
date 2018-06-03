0d801cad7bd70f8fc7aabdb64446c252efae0013
garouttc <39816619+garouttc@users.noreply.github.com>
2018-06-03 12:38:30 -0700

    Update checkLoginStatus.js

--- checkLoginStatus.js
+++ checkLoginStatus.js
@@ -5,7 +5,7 @@ function submitLoginForm()
   var password = document.getElementById("login-form").elements["password"].value;
   
   // Show the searching, finding, and view pet profiles options
-  document.getElementById("jdiv").innerHTML = '<button id="btn_download"class="btn btn-primary btn btn-lg">Search for Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">Report a Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">My Pet Profile</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>';
+  document.getElementById("jdiv").innerHTML = '<h1>UNLEASHED PET FINDER: </h1>\n<h3>Please choose option</h3>\n<div class="col-sm-12" id="jumbotron-content">\n<button id="btn_download"class="btn btn-primary btn btn-lg">Search for Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open('/download');})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">Report a Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open('/download');})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">My Pet Profile</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open('/download');})</script>\n</div>\n<br/>';
   
   // Show navbar options again
   // Hide navbar options that shouldn't be accessed from users who are not logged in
@@ -38,7 +38,7 @@ window.onload = function()
   if (sessionStorage.getItem("username"))
   {
     // Show the searching, finding, and view pet profiles options
-    document.getElementById("jdiv").innerHTML = '<button id="btn_download"class="btn btn-primary btn btn-lg">Search for Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">Report a Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">My Pet Profile</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>';
+    document.getElementById("jdiv").innerHTML = '<h1>UNLEASHED PET FINDER: </h1>\n<h3>Please choose option</h3>\n<div class="col-sm-12" id="jumbotron-content">\n<button id="btn_download"class="btn btn-primary btn btn-lg">Search for Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open('/download');})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">Report a Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open('/download');})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">My Pet Profile</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open('/download');})</script>\n</div>\n<br/>';
     
   }
   // If a user is not logged in
@@ -52,7 +52,7 @@ window.onload = function()
       liNodes[i].style.display = 'none';
     }
     // Show login prompt
-    document.getElementById("jdiv").innerHTML = '<form id="login-form">\nUsername <input type="text" name="username">\nPassword <input type="password" name="password">\n<input id="submit" type="button" name="submit" value="Submit" onclick="submitLoginForm()">\n</form>';
+    document.getElementById("jdiv").innerHTML = '<h1>UNLEASHED PET FINDER</h1>\n<form id="login-form">\nUsername <input type="text" name="username">\nPassword <input type="password" name="password">\n<input id="submit" type="button" name="submit" value="Submit" onclick="submitLoginForm()">\n</form>';
 
   }
 }
