a453b6412d61025f00792d9b0085571d0bda545d
garouttc <39816619+garouttc@users.noreply.github.com>
2018-06-03 12:27:32 -0700

    Update checkLoginStatus.js

--- checkLoginStatus.js
+++ checkLoginStatus.js
@@ -22,7 +22,6 @@ function submitLoginForm()
   sessionStorage.setItem("username", username);
   sessionStorage.setItem("password", password);
  
-  
 }
 
 window.onload = function()
@@ -39,14 +38,14 @@ window.onload = function()
   if (sessionStorage.getItem("username"))
   {
     // Show the searching, finding, and view pet profiles options
-    document.getElementById("jumbotron-content").innerHTML = '<button id="btn_download"class="btn btn-primary btn btn-lg">Search for Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">Report a Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">My Pet Profile</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>';
+    document.getElementById("jdiv").innerHTML = '<button id="btn_download"class="btn btn-primary btn btn-lg">Search for Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">Report a Lost Pet</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>\n<button id="btn_download"class="btn btn-primary btn btn-lg">My Pet Profile</button>\n<script type="text/javascript">$("#btn_download").click(function(){window.open("/download");})</script>';
     
   }
   // If a user is not logged in
   else
   {
     // Show login prompt
-    document.getElementById("jumbotron-content").innerHTML = '<form id="login-form">\nUsername <input type="text" name="username">\nPassword <input type="password" name="password">\n<input id="submit" type="button" name="submit" value="Submit">\n</form>';
+    document.getElementById("jdiv").innerHTML = '<form id="login-form">\nUsername <input type="text" name="username">\nPassword <input type="password" name="password">\n<input id="submit" type="button" name="submit" value="Submit">\n</form>';
     document.getElementById("submit").addEventListener("click", submitLoginForm());
     // Hide navbar options that shouldn't be accessed from users who are not logged in
     var liNodes = document.getElementById("bs-navbar-collapse-main").getElementsByTagName("li");
