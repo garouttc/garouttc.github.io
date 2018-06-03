bffd57d3b171d2d3ec6e71c63e252ca74068adbd
garouttc <39816619+garouttc@users.noreply.github.com>
2018-06-03 12:21:14 -0700

    Update checkLoginStatus.js

--- checkLoginStatus.js
+++ checkLoginStatus.js
@@ -47,7 +47,7 @@ window.onload = function()
   {
     // Show login prompt
     document.getElementById("jumbotron-content").innerHTML = '<form id="login-form">\nUsername <input type="text" name="username">\nPassword <input type="password" name="password">\n<input id="submit" type="button" name="submit" value="Submit">\n</form>';
-    document.getElementById("submit").addEventListener("click", submitLoginForm());
+    //document.getElementById("submit").addEventListener("click", submitLoginForm());
     // Hide navbar options that shouldn't be accessed from users who are not logged in
     var liNodes = document.getElementById("bs-navbar-collapse-main").getElementsByTagName("li");
     var i;
