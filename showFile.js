51791ab731a30f95c967d624f163f10ff759fbd8
Joshua Padiernos <35288552+JoshuaPadiernos@users.noreply.github.com>
2018-06-03 17:44:53 -0700

    Add files via upload

---
+++ showFile.js
@@ -0,0 +1,11 @@
+function showFile() {
+	if(this.files && this.files[0]) {
+  	var obj = new FileReader();
+    obj.onload = function(data) {
+    	var image = document.getElementById("image");
+      image.src = data.target.result;
+      image.style.display = "block";
+    }
+    obj.readAsDataURL(this.files[0]);
+  }
+}
