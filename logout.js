aa5576a9402e8ae5f3b1cc8df6a77abbaf36f60e
garouttc <39816619+garouttc@users.noreply.github.com>
2018-06-05 16:44:17 -0700

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,4 +1,14 @@
-document.onload = function()
+function logout() 
 {
-
+  // First check if the browser supports session storage
+  // This step should be unnecessary since we handle this in checkLoginStatus...but I guess it can't hurt
+  if (typeof(Storage) == "undefined")
+  {
+    // Redirect to error page
+    window.location = "https://garouttc.github.io/error.html";
+  }
+  else
+  {
+    sessionStorage.clear();  
+  }
 }
