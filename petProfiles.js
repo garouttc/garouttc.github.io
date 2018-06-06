074c483b68e75fdb1d00fab66bce336c20f3bd37
garouttc <39816619+garouttc@users.noreply.github.com>
2018-06-05 19:50:12 -0700

    Display pets nested for loops skeleton

--- petProfiles.js
+++ petProfiles.js
@@ -3,12 +3,27 @@ window.onload = function()
   // Say hello to the logged-in user
   var helloString = "Hello ";
   helloString = helloString.concat(sessionStorage.getItem("username"));
-  helloString = helloString.concat("! Here are your pet profiles:");
+  helloString = helloString.concat("! Here are your pet profiles.");
   var text = document.createTextNode(helloString);
   console.log(helloString);
   document.getElementById("hello-user").appendChild(text);
 
-  // Check the user's pet profile JSON object and display any pet profiles they have 
-
+  // Get the user's JSON pet list from storage
+  var petsStorage = sessionStorage.getItem("pets");
+  if (petsStorage)
+  {
+    pets = JSON.parse(petsStorage);
+  }
+  
+  // Display pet profiles
+  for (i = 0; i < pets.length; i++)
+  {
+    var pet = pets[i];
+    for (var attribute in pet)
+    {
+      
+    }
+  }
+  
   // Add button for creating a new pet profile 
 }
