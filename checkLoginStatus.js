4e29c48bf2f64922ae99f93ff5afd861541eea22
garouttc <39816619+garouttc@users.noreply.github.com>
2018-06-05 17:08:20 -0700

    Update checkLoginStatus.js

--- checkLoginStatus.js
+++ checkLoginStatus.js
@@ -60,6 +60,7 @@ window.onload = function()
     // Create a new div with login prompt and attach to jumbotron
     var div = document.createElement("div");
     div.innerHTML = '<h1>UNLEASHED PET FINDER</h1>\n<form id="login-form">\nUsername <input type="text" name="username">\nPassword <input type="password" name="password">\n<input id="submit" type="button" name="submit" value="Submit" onclick="submitLoginForm()">\n</form>';
-    document.getElementById("jcontainer").appendChild(div);
+    var jcontainer = document.getElementById("jcontainer");
+    jcontainer.insertBefore(div, jcontainer.childNodes[0]);
   }
 }
