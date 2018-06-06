e93fd2f95136a2069157aceeb16d52a96e67c2d3
garouttc <39816619+garouttc@users.noreply.github.com>
2018-06-05 19:32:38 -0700

    Update petProfiles.js

--- petProfiles.js
+++ petProfiles.js
@@ -1,5 +1,12 @@
 window.onload = function()
 {
+  // Say hello to the logged-in user
+  var helloString = "Hello ";
+  helloString = helloString.concat(sessionStorage.getItem("username"));
+  helloString = helloString.concat("! Here are your pet profiles:");
+  document.getElementById("hello-user").appendChild(helloString);
 
+  // Check the user's pet profile JSON object and display any pet profiles they have 
 
+  // Add button for creating a new pet profile 
 }
