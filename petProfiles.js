c33b30f47ca36eba62303c51843fa933ab5ac288
garouttc <39816619+garouttc@users.noreply.github.com>
2018-06-06 21:07:46 -0700

    Update petProfiles.js

--- petProfiles.js
+++ petProfiles.js
@@ -18,11 +18,56 @@ function displayPetProfiles()
   // Display pet profiles
   for (i = 0; i < pets.length; i++)
   {
+    // Create a div for each pet and attach it to the jumbotron
+    var div = document.createElement("div");
+    div.className = "col-sm-12";
+    var id = "jumbotron-content";
+    id = id.concat(i);
+    div.id = id;
+    document.getElementById("jumbotron").appendChild(div);
     var pet = pets[i];
-    for (var attribute in pet)
-    {
-      
-    }
+    
+    // Display pet name as a header for profile
+    var nameHeader = document.createElement("h3");
+    var text = "Pet: ";
+    text.concat(pet.name);
+    nameHeader.appendChild(document.createTextNode(text));
+    div.appendChild(nameHeader);
+    
+    // Then list its attributes
+    // Species
+    var list = document.createElement("ul");
+    var attr = document.createElement("li");
+    attr.style.list-style-type = "none";
+    var attrText = "Species: ";
+    attrText.concat(pet.species);
+    attr.appendChild(document.createTextNode(attrText));
+    list.appendChild(attr);
+    
+    // Breed
+    attr = document.createElement("li");
+    attr.style.list-style-type = "none";
+    attrText = "Breed: ";
+    attrText.concat(pet.breed);
+    attr.appendChild(document.createTextNode(attrText));
+    list.appendChild(attr);
+    
+    // Gender
+    attr = document.createElement("li");
+    attr.style.list-style-type = "none";
+    attrText = "Gender: ";
+    attrText.concat(pet.gender);
+    attr.appendChild(document.createTextNode(attrText));
+    list.appendChild(attr);
+    
+    // Gender
+    attr = document.createElement("li");
+    attr.style.list-style-type = "none";
+    attrText = "Color: ";
+    attrText.concat(pet.color);
+    attr.appendChild(document.createTextNode(attrText));
+    list.appendChild(attr);
+    
   }
   
   // Add button for creating a new pet profile 
