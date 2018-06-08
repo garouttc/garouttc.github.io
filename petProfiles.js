7cc362d964900f7170943b6eff9811368590c5ae
garouttc <39816619+garouttc@users.noreply.github.com>
2018-06-07 21:02:09 -0700

    Update petProfiles.js

--- petProfiles.js
+++ petProfiles.js
@@ -79,4 +79,4 @@ function displayPetProfiles()
 }
 
 
-//document.addEventListener(document.getElementById("jdiv").onchange, displayPetProfiles());
+document.addEventListener("DOMContentLoaded", displayPetProfiles());
