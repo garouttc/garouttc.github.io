04f869ffd76e942b38dca8c90523393687eee461
Joshua Padiernos <35288552+JoshuaPadiernos@users.noreply.github.com>
2018-06-08 13:55:55 -0700

    Add files via upload

---
+++ alertSearchRedirect.js
@@ -0,0 +1,8 @@
+
+document.getElementById("redirect").addEventListener("click", myAlert);
+
+function myAlert() {
+    alert("Success! Retrieving search results now.");
+    window.location = 'search-results.html';
+
+}
