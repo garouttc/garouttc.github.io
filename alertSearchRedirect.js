dfafb557bf1998250a4e2ebb0954e71addf21207
Joshua Padiernos <35288552+JoshuaPadiernos@users.noreply.github.com>
2018-06-08 13:56:28 -0700

    Update alertSearchRedirect.js

--- alertSearchRedirect.js
+++ alertSearchRedirect.js
@@ -3,6 +3,6 @@ document.getElementById("redirect").addEventListener("click", myAlert);
 
 function myAlert() {
     alert("Success! Retrieving search results now.");
-    window.location = 'search-results.html';
+    window.location = '/search-results.html';
 
 }
