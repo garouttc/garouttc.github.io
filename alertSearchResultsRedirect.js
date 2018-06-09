1b4af709f77239579348b85cd6c6c829c077a756
Joshua Padiernos <35288552+JoshuaPadiernos@users.noreply.github.com>
2018-06-08 18:08:46 -0700

    Update alertSearchResultsRedirect.js

--- alertSearchResultsRedirect.js
+++ alertSearchResultsRedirect.js
@@ -1,10 +1,6 @@
-document.getElementById("redirect").addEventListener("click", myAlert);
-
 function myAlert() {
     if (window.confirm("Is this your pet?")) {
         alert("Redirecting you to contact the pet finder!");
         window.location = '/contact.html';
     }
  }
-
-
