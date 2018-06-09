ba0b9507fffff9a1af1a61338f438fedca502b7e
Joshua Padiernos <35288552+JoshuaPadiernos@users.noreply.github.com>
2018-06-08 18:00:24 -0700

    Update alertContactRedirect.js

--- alertContactRedirect.js
+++ alertContactRedirect.js
@@ -4,3 +4,21 @@ function myAlert() {
     alert("Message Sent!");
     window.location = '/';
 }
+
+function displayPetProfiles()
+{
+  if (!sessionStorage.getItem("username"))
+  {
+    window.location = "https://garouttc.github.io/index.html";
+  }
+  
+  // Say hello to the logged-in user
+  var helloString = "Hello ";
+  helloString = helloString.concat(sessionStorage.getItem("username"));
+  helloString = helloString.concat("! Here is the contact for the pet finder user, iLuvAnimalz.");
+  var text = document.createTextNode(helloString);
+  console.log(helloString);
+  document.getElementById("hello-user").appendChild(text);
+}
+
+document.addEventListener("DOMContentLoaded", displayPetProfiles());
